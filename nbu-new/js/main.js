c005f97be009aa8e4c92a7ab34207be4e297fd83
timmyru <33581137+timmyru@users.noreply.github.com>
2019-12-09 02:53:13 +0300

    Add files via upload

--- nbu-new/js/main.js
+++ nbu-new/js/main.js
@@ -41,51 +41,6 @@ $(document).ready(function(){
     });
 });
 
-//video
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player1;
-
-function onYouTubeIframeAPIReady() {
-
-    if($(window).width() < 576) {
-        player1 = new YT.Player('divPlayer1', {
-            height: '350',
-            width: '100%',
-            videoId: 'd8Oc90QevaI',
-        });
-    } else if ($(window).width() < 768) {
-        player1 = new YT.Player('divPlayer1', {
-            height: '400',
-            width: '100%',
-            videoId: 'd8Oc90QevaI',
-        });
-    } else if ($(window).width() < 992) {
-        player1 = new YT.Player('divPlayer1', {
-            height: '450',
-            width: '100%',
-            videoId: 'd8Oc90QevaI',
-        });
-    } else {
-        player1 = new YT.Player('divPlayer1', {
-            height: '550',
-            width: '100%',
-            videoId: 'd8Oc90QevaI',
-        });
-    }
-
-}
-
-function play1(event) {
-    $('.video-wrapper').hide();
-    $('#divPlayer1').show();
-    $('iframe').css('display', 'block');
-    player1.playVideo();
-}
 
 //бургер-меню
 let burger = document.body.querySelector('.header-burger');
@@ -93,3 +48,11 @@ burger.addEventListener('click', function () {
    burger.classList.toggle('burger-active');
     $('.header-burger__menu').slideToggle();
 });
+
+
+//отзывы
+// let reviewBtn = document.body.querySelector('.footer-bottom__right');
+// reviewBtn.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     $('.reviews').slideToggle();
+// });
