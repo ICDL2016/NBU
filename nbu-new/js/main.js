4bec150560337b312d37effdfa253be8f3de64ad
timmyru <33581137+timmyru@users.noreply.github.com>
2020-01-25 22:56:29 +0300

    Add files via upload

--- nbu-new/js/main.js
+++ nbu-new/js/main.js
@@ -39,72 +39,79 @@ $(document).ready(function () {
             },
         }
     });
-});
+
+    window.onresize = function () {
+        if ($(window).width() < 768) {
+            $('.onecourse-offer__wrapper-videoOn, .prof-offer__underVideo').hide();
+        }
+    };
 
 
 //бургер-меню
-let burger = document.body.querySelector('.header-burger');
-burger.addEventListener('click', function () {
-    burger.classList.toggle('burger-active');
-    $('.header-burger__menu').slideToggle();
-});
+    let burger = document.body.querySelector('.header-burger');
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('burger-active');
+        $('.header-burger__menu').slideToggle();
+    });
 
 
 // псевдовалидация
 
-let emptyTop = false;
-let emptyBottom = false;
-$('.form-input__name').on('click', function () {
-    emptyTop = true;
-    if (emptyBottom === true && ($(this).parent().children('.form-input__email').val().length === 0 || $(this).parent().children('.form-input__email').val().indexOf('@') === -1)) {
-        $(this).parent().children('.form-input__span-email').show();
-    }
-}).on('input', function () {
-    $(this).parent().children('.form-input__span-name').hide();
-    $(this).parent().children('.form-input__name').removeClass('border-red');
-});
+    let emptyTop = false;
+    let emptyBottom = false;
+    $('.form-input__name').on('click', function () {
+        emptyTop = true;
+        if (emptyBottom === true && ($(this).parent().children('.form-input__email').val().length === 0 || $(this).parent().children('.form-input__email').val().indexOf('@') === -1)) {
+            $(this).parent().children('.form-input__span-email').show();
+        }
+    }).on('input', function () {
+        $(this).parent().children('.form-input__span-name').hide();
+        $(this).parent().children('.form-input__name').removeClass('border-red');
+    });
 
-$('.form-input__email').on('click', function () {
-    emptyBottom = true;
-    if (emptyTop === true && $(this).parent().children('.form-input__name').val().length === 0) {
-        $(this).parent().children('.form-input__span-name').show();
-    }
-}).on('input', function () {
-    $(this).parent().children('.form-input__span-email').hide();
-    $(this).parent().children('.form-input__email').removeClass('border-red');
-});
+    $('.form-input__email').on('click', function () {
+        emptyBottom = true;
+        if (emptyTop === true && $(this).parent().children('.form-input__name').val().length === 0) {
+            $(this).parent().children('.form-input__span-name').show();
+        }
+    }).on('input', function () {
+        $(this).parent().children('.form-input__span-email').hide();
+        $(this).parent().children('.form-input__email').removeClass('border-red');
+    });
 
-$('.popup-form__submit').on('click', function () {
-    event.preventDefault();
-    let send = true;
+    $('.popup-form__submit').on('click', function () {
+        event.preventDefault();
+        let send = true;
 
-    if ($(this).parent().children('.form-input__name').val().length === 0) {
-        $(this).parent().children('.form-input__span-name').show();
-        $(this).parent().children('.form-input__name').addClass('border-red');
-        send = false;
-    }
+        if ($(this).parent().children('.form-input__name').val().length === 0) {
+            $(this).parent().children('.form-input__span-name').show();
+            $(this).parent().children('.form-input__name').addClass('border-red');
+            send = false;
+        }
 
-    if ($(this).parent().children('.form-input__email').val().length === 0 || $(this).parent().children('.form-input__email').val().indexOf('@') === -1) {
-        $(this).parent().children('.form-input__span-email').show();
-        $(this).parent().children('.form-input__email').addClass('border-red');
-        send = false;
-    }
+        if ($(this).parent().children('.form-input__email').val().length === 0 || $(this).parent().children('.form-input__email').val().indexOf('@') === -1) {
+            $(this).parent().children('.form-input__span-email').show();
+            $(this).parent().children('.form-input__email').addClass('border-red');
+            send = false;
+        }
 
-    if (send === true) {
-        $(this).parent().submit();
-    }
-});
+        if (send === true) {
+            $(this).parent().submit();
+        }
+    });
 
 //открытие формы-модалки
-$('.show-form__btn, .video-button, .start-button').on('click', function (e) {
-    event.preventDefault();
-    $('.overlay').fadeIn();
-    $('.popup').fadeIn();
-    $('body').css('overflow', 'hidden');
-});
+    $('.show-form__btn, .video-button, .start-button').on('click', function (e) {
+        event.preventDefault();
+        $('.overlay').fadeIn();
+        $('.popup').fadeIn();
+        $('body').css('overflow', 'hidden');
+    });
 
-$('.overlay, .popup-close').on('click', function () {
-    $('.overlay').fadeOut();
-    $('.popup').fadeOut();
-    $('body').css('overflow', 'visible');
+    $('.overlay, .popup-close').on('click', function () {
+        $('.overlay').fadeOut();
+        $('.popup').fadeOut();
+        $('body').css('overflow', 'visible');
+    });
+
 });
